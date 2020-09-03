299032e7cd81fdd31e2d2a36518e855ed504368d
Jzzzgg <45835787+Jzzzgg@users.noreply.github.com>
2020-09-03 17:36:28 -0400

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -32,7 +32,7 @@ export default {
     },
     {
       name: 'Github Repo',
-      link: 'https://github.com/danielkellyio/awake-template',
+      link: 'https://github.com/BergenSwampExperience/bergenSwampAlt',
       target: '_blank'
     },
     {
