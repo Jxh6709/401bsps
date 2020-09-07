883faacaf581c73da6d822631b5a8bb82d3d457a
Jordan Darling <jxd2395@rit.edu>
2020-09-06 20:14:48 -0400

    forgot about thing

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
       title="About Us"
-      subtitle="Example About Page"
+      subtitle="Conservation &amp; Education"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
