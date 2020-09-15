6608cdeb8aee267717ee022f9e73e208bcab71aa
Joshua Haber <jxh6709@rit.edu>
2020-09-15 17:28:11 -0400

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -51,7 +51,7 @@ export default {
   disqus: {
     on: true,
     loadingStrategy: 'button', // Options: onload, lazy, button
-    siteShortName: 'blog-bergenswamp-io' // 'blog-danielkelly-io'
+    siteShortName: 'bergenswamp-netlify-app' // 'blog-danielkelly-io'
   },
 
   // Newsletter Subscribe
