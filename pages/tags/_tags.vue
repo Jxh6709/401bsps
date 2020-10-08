ddb703e2a2ae7d92b7c2e6c805208e0de3c86f0a
Joshua Haber <jxh6709@rit.edu>
2020-10-08 12:58:12 -0400

    this might work but will likely fail

--- pages/tags/_tags.vue
+++ pages/tags/_tags.vue
@@ -1,33 +1,21 @@
  gcn<template>
   <div id="category-page" class="page-wrapper category-page">
-    <site-hero
-      :title="tagname"
-      :subtitle="$store.state.content"
-      :image="$store.state.image"
-    />
+    <site-hero :title="tagname" :subtitle="$store.state.content" :image="$store.state.image" />
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <!-- Posts in Category -->
-        <posts-grid :category="[$store.state.name]" :per-row="2" />
+        <!-- <posts-grid :category="[$store.state.name]" :per-row="2" /> -->
+        <post-card v-for="item in taggedObjs" :key="item.title"
+          :title="item.title"
+          :link="item.slug ? `/${item.slug}` : ''"
+          :image="item.featureImage"
+          :author="item.author"
+          :date="item.date"
+          :tags="item.tags"
+          :trails="item.trails"
+        />
       </template>
-<!--      <template v-slot:sidebar>
-        <h3 class="subtitle">
-          All Categories
-        </h3>
-        <div class="panel">
-          <nuxt-link
-            v-for="cat in allCats"
-            :key="cat.slug" :to="`/categories/${cat.slug}`"
-            :class="{
-              'panel-block': true,
-              'is-active': cat.slug === $route.params.single
-            }"
-          >
-            {{ cat.name }}
-          </nuxt-link>
-        </div>
-      </template> -->
-     </main-section>
+    </main-section>
   </div>
 </template>
 <script>
@@ -38,21 +26,31 @@ export default {
       allCats: []
     }
   },
-  computed : {
-  	tagname : ()=>{
-  		return decodeURI(window.location.pathname).split("/tags/")[1]
-  	}
+  computed: {
+    tagname: () => {
+      return decodeURI(window.location.pathname).split('/tags/')[1]
+    },
+    taggedPosts: () => {
+      let taggedObjs = []
+      const len = this.allPosts.length
+      for (i = 0; i < len; i++) {
+        if (this.allPosts[i].trails.includes(tagname))
+          taggedObjs.push(this.allPosts[i])
+      }
+      return taggedObjs
+    }
   },
   fetch({ store, params }) {
-    console.log("store");
-    console.log(store);
-    console.log("Params");
-    console.log(params);
+    console.log('store')
+    console.log(store)
+    console.log('Params')
+    console.log(params)
     setPageData(store, { resource: 'category', slug: params.single })
   },
   async created() {
     this.allCats = await this.$cms.category.getAll()
-    console.log(this.$store);
+    this.allPosts = await this.$cms.post.getAll()
+    console.log(this.$store)
     console.log(await this.$cms.post.getAll())
     console.log(await this.$cms.trail.getAll())
   }
