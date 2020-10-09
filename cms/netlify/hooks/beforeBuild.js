4a602d6cbbf9e3bc7e597de306816492f28f7ef5
Joshua Haber <jxh6709@rit.edu>
2020-10-08 23:19:41 -0400

    Update beforeBuild.js

--- cms/netlify/hooks/beforeBuild.js
+++ cms/netlify/hooks/beforeBuild.js
@@ -1,7 +1,7 @@
 import generatePostApi from '../build/generate-post-api'
 import generateCategoryApi from '../build/generate-category-api'
+import generateTrailApi from '../build/generate-trail-api'
 import copyStaticToDist from '../build/copy-static-to-dist'
-import generateTrailApi from '../build/generateTrailApi'
 
 export default () => {
   generatePostApi()
